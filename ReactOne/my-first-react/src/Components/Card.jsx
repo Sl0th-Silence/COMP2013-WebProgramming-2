5625ccc4356d48d65e906b4c669463aee6639088
Jay Mills <jay.mills@student.sl.on.ca>
2025-09-17 09:56:16 -0400

    Added Diamond world

---
+++ ReactOne/my-first-react/src/Components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card(props)
+{
+    return <div className="Card-Component">
+        <img src={props.image} alt="" width='100px'/>
+        <h2>{props.name}</h2>
+        <p>{props.price}</p>
+    </div>
+}
