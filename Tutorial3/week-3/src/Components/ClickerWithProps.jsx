57a010b04189bf3a49ab9b6306e63f76ca80962f
Jay Mills <73972373+Sl0th-Silence@users.noreply.github.com>
2025-09-25 11:49:31 -0400

    Delete Tutorial3/week-3 directory

--- Tutorial3/week-3/src/Components/ClickerWithProps.jsx
+++
@@ -1,9 +0,0 @@
-//This is an event on click function aka handler
-
-const messageOnClick = (msg) => console.log(msg);
-
-export default function ClickerWithProps({message, btnText}){
-    // if a handler has a param and needs an argument to work,
-    // we must use a LAMBDA function inside the event, ie, onclick, onhover, on...etc.
-    return <button onClick={() => messageOnClick(message)}>{btnText}</button>
-}
